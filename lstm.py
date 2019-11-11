24136382d1f812fff1b3f713f414373c4b91ada6
thanos-sakelliou <thanos.sakelliou@gmail.com>
2019-11-11 20:42:58 +0100

    CuDNNLSTM speedup for Colab GPU

--- lstm.py
+++ lstm.py
@@ -87,15 +87,15 @@ def prepare_sequences(notes, n_vocab):
 def create_network(network_input, n_vocab):
     """ create the structure of the neural network """
     model = Sequential()
-    model.add(LSTM(
+    model.add(CuDNNLSTM(
         512,
         input_shape=(network_input.shape[1], network_input.shape[2]),
         return_sequences=True
     ))
     model.add(Dropout(0.3))
-    model.add(LSTM(512, return_sequences=True))
+    model.add(CuDNNLSTM(512, return_sequences=True))
     model.add(Dropout(0.3))
-    model.add(LSTM(512))
+    model.add(CuDNNLSTM(512))
     model.add(Dense(256))
     model.add(Dropout(0.3))
     model.add(Dense(n_vocab))
