fd591c2ddf8df0f82a36000483fdd2460d494438
thanos-sakelliou <thanos.sakelliou@gmail.com>
2019-11-11 21:20:17 +0100

    import

--- lstm.py
+++ lstm.py
@@ -7,7 +7,7 @@ from music21 import converter, instrument, note, chord
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
-from keras.layers import LSTM
+from keras.layers import LSTM, CuDNNLSTM
 from keras.layers import Activation
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
